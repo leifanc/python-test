9f535eaea492e8a2ece324d0ddcf2247f390d28c
leifanc <1372232447@qq.com>
2015-02-21 17:28:48 +0800

    finnal

---
+++ pool_test.py
@@ -0,0 +1,12 @@
+from multiprocessing import Pool
+import os, time, random
+
+def run_child(name):
+    print 'i am child process %s(%s)' %(name, os.getpid())
+    time.sleep(random.random() * 3)
+p = Pool()
+for i in range(4):
+    p.apply_async(run_child, args=(i,))
+p.close()
+p.join()
+print 'all end'
